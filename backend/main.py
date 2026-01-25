424ec14e98812bf36d42130950d607bef5c91e75
Ibrahima Cisse <ibrahima.cisse@sura.health>
2026-01-25 18:34:03 +0000

    feat: update CORS configuration to allow additional origins

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://raceschedules.vercel.app/", "https://raceschedules.ibrahima.dev/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
